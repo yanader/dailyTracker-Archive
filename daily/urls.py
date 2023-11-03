0bf1f1d2770db1f5cfd6431e0cb13b7974cd2e50
Ste <mrbarrow@hotmail.co.uk>
2023-11-03 11:44:38 +0000

    set up calendar&data views

--- daily/urls.py
+++ daily/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('/calendar', views.calendar, name="calendar"),
+    path('/data', views.data, name="data"),
 ]
--- daily/views.py
+++ daily/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "daily/index.html")
+
+def calendar(request):
+    return render(request, "daily/calendar.html")
+
+def data(request):
+    return render(request, "daily/data.html")
